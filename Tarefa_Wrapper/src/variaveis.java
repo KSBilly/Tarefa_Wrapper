c186dbbc8ec3ced7da38993af64c13c2f295198f
Leonardo <leonardo@aulas.com>
2025-10-07 16:33:38 -0300

    Tarefa_Wrapper

---
+++ Tarefa_Wrapper/src/variaveis.java
@@ -0,0 +1,8 @@
+public class variaveis {
+    public static void main(String[] args) {
+        int idade = 18;
+        System.out.println(idade);
+        long idadeNova = idade;
+        System.out.println(idadeNova);
+    }
+}
