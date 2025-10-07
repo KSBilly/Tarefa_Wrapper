de6826595a09289a092567e15565e7c5f07f3a21
Leonardo <leonardo@aulas.com>
2025-10-07 16:43:31 -0300

    Correção da atividade

--- Tarefa_Wrapper/src/variaveis.java
+++ Tarefa_Wrapper/src/variaveis.java
@@ -2,7 +2,7 @@ public class variaveis {
     public static void main(String[] args) {
         int idade = 18;
         System.out.println(idade);
-        long idadeNova = idade;
+        Integer idadeNova = idade;
         System.out.println(idadeNova);
     }
 }
